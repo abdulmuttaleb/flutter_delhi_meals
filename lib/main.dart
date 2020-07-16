6c35f74c3958c1073c7e68182571b4761f033c50
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-07-16 22:11:43 +0200

    adding fonts, styling themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:deli_meals/screens/categories_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -11,10 +12,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+          bodyText1: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          bodyText2: TextStyle(
+            color: Color.fromRGBO(20, 51, 51, 1)
+          ),
+          headline6: TextStyle(
+            fontSize: 24,
+            fontFamily: 'RobotoCondensed'
+          )
+        ),
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(),
+      home: CategoriesScreen(),
     );
   }
 }
--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -5,13 +5,19 @@ import 'package:flutter/material.dart';
 class CategoriesScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat.title, cat.color)).toList(),
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deli Meal'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat.title, cat.color)).toList(),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
       ),
     );
   }
