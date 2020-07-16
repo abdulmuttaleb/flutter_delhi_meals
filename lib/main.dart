0ff3d9455b939f6e84ac595dbcdd93780d596c61
AbdulMuttaleb <a.m.abdelmottaleb@gmail.com>
2020-07-16 21:03:34 +0200

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget{
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Deli Meals'),
+    );
+  }
+
+}
